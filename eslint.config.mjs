fa8781f6a1c5b8cfd8a9e8d39552500143ede3bd
Andres Manz <manz.andres@gmail.com>
2025-04-09 15:36:13 +0200

    Add eslint configs for unused vars and empty functions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,17 @@ export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
+    {
+        rules: {
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
 );
