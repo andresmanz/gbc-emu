e03a8880ad592eac37454307a4c48422bbdc2ef6
Andres Manz <manz.andres@gmail.com>
2025-04-10 17:00:53 +0200

    Implement NOP and ADD A, r8

--- src/cpu/cpu.test.ts
+++ src/cpu/cpu.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { Cpu } from './cpu';
+import { Cpu, R8Register, r8Registers } from './cpu';
 import { MemoryBus } from '../memory/memoryBus';
 import { Rom } from '../memory/rom';
 
@@ -12,6 +12,10 @@ class MockMemoryBus implements MemoryBus {
 
     setRom(_rom: Rom): void {}
 
+    setRomData(data: Uint8Array): void {
+        this.memory.set(data, 0x0100);
+    }
+
     read(address: number): number {
         return this.memory[address];
     }
@@ -19,6 +23,65 @@ class MockMemoryBus implements MemoryBus {
     write(address: number, value: number): void {
         this.memory[address] = value;
     }
+}
+
+interface Flags {
+    z?: number;
+    n?: number;
+    h?: number;
+    c?: number;
+}
+
+interface ArithmeticOpTestCase {
+    opcode: number;
+    aValue: number;
+    r8Register: R8Register;
+    r8Value: number;
+    expected: number;
+    flags?: Flags;
+    description?: string;
+}
+
+function testArithmeticOp({
+    opcode,
+    aValue,
+    r8Register,
+    r8Value,
+    expected,
+    flags = {},
+    description,
+}: ArithmeticOpTestCase) {
+    const label =
+        description ??
+        `A=0x${aValue.toString(16)}, r8=0x${r8Value.toString(16)}`;
+
+    it(label, () => {
+        const rom = new Uint8Array([opcode]);
+        const cpu = setupWithRom(rom);
+
+        cpu.registers.a = aValue;
+        cpu.setR8Value(r8Register, r8Value);
+
+        cpu.step();
+
+        expect(cpu.registers.a).toBe(expected);
+
+        if (flags.z !== undefined) {
+            expect(cpu.registers.zeroFlag).toBe(flags.z);
+        }
+
+        if (flags.n !== undefined) {
+            expect(cpu.registers.subtractFlag).toBe(flags.n);
+        }
+
+        if (flags.h !== undefined) {
+            expect(cpu.registers.halfCarryFlag).toBe(flags.h);
+        }
+
+        if (flags.c !== undefined) {
+            expect(cpu.registers.carryFlag).toBe(flags.c);
+        }
+    });
 }
 
 describe('initially', () => {
@@ -30,5 +93,117 @@ describe('initially', () => {
     it('sets SP to 0xfffe', () => {
         const cpu = new Cpu(new MockMemoryBus(16));
         expect(cpu.registers.sp).toBe(0xfffe);
+    });
+});
+
+/**
+ * Creates a CPU instance with a mock memory bus and loads the given ROM data.
+ * The ROM data is written to the memory bus starting at address 0x0100, since
+ * the CPU starts executing instructions from this address.
+ */
+function setupWithRom(romData: Uint8Array) {
+    const memoryBus = new MockMemoryBus(0x100 + romData.length);
+    memoryBus.setRomData(romData);
+
+    const cpu = new Cpu(memoryBus);
+    return cpu;
+}
+
+it('handles nop opcode', () => {
+    const romData = new Uint8Array([0x00]);
+    const cpu = setupWithRom(romData);
+
+    cpu.step();
+
+    expect(cpu.registers.pc).toBe(0x0101);
+});
+
+const r8RegistersWithoutA = r8Registers.filter(r => r !== 'a');
+
+describe('add a, r8', () => {
+    describe.for(r8RegistersWithoutA)('add a, %s', r8 => {
+        const opcode = 0x80 + r8Registers.indexOf(r8);
+
+        testArithmeticOp({
+            description: 'adds the value of the register to A',
+            opcode,
+            aValue: 0x01,
+            r8Register: r8,
+            r8Value: 0x02,
+            expected: 0x03,
+        });
+
+        testArithmeticOp({
+            description: 'adds the value of the register to A with carry',
+            opcode,
+            aValue: 0xff,
+            r8Register: r8,
+            r8Value: 0x02,
+            expected: 0x01,
+            flags: { z: 0, c: 1 },
+        });
+
+        testArithmeticOp({
+            description: 'handles overflow correctly',
+            opcode,
+            aValue: 0x01,
+            r8Register: r8,
+            r8Value: 0xff,
+            expected: 0x00,
+            flags: { z: 1, c: 1 },
+        });
+
+        testArithmeticOp({
+            description: 'sets the zero flag when result is zero',
+            opcode,
+            aValue: 0x10,
+            r8Register: r8,
+            r8Value: 0xf0,
+            expected: 0x00,
+            flags: { z: 1, c: 1 },
+        });
+    });
+
+    describe('add a, a', () => {
+        const opcode = 0x87;
+
+        testArithmeticOp({
+            description: 'adds the value of A to itself',
+            opcode,
+            aValue: 0x01,
+            r8Register: 'a',
+            r8Value: 0x01,
+            expected: 0x02,
+        });
+
+        testArithmeticOp({
+            description: 'adds the value of A to itself with carry',
+            opcode,
+            aValue: 0xff,
+            r8Register: 'a',
+            r8Value: 0xff,
+            expected: 0xfe,
+            flags: { z: 0, c: 1 },
+        });
+
+        testArithmeticOp({
+            description: 'handles overflow correctly',
+            opcode,
+            aValue: 0x80,
+            r8Register: 'a',
+            r8Value: 0x80,
+            expected: 0x00,
+            flags: { z: 1, c: 1 },
+        });
+
+        testArithmeticOp({
+            description: 'sets the zero flag when result is zero',
+            opcode,
+            aValue: 0x0,
+            r8Register: 'a',
+            r8Value: 0x0,
+            expected: 0x00,
+            flags: { z: 1, c: 0 },
+        });
     });
 });
--- src/cpu/cpu.ts
+++ src/cpu/cpu.ts
@@ -1,13 +1,18 @@
 import { MemoryBus } from '../memory/memoryBus';
 import { CpuRegisters } from './cpuRegisters';
 
+export const r8Registers = ['b', 'c', 'd', 'e', 'h', 'l', '[hl]', 'a'] as const;
+export type R8Register = (typeof r8Registers)[number];
+
 export class Cpu {
     public memoryBus: MemoryBus;
     public registers = new CpuRegisters();
+    private opcodeTable = new Map<number, (cpu: Cpu) => void>();
 
     constructor(memoryBus: MemoryBus) {
         this.memoryBus = memoryBus;
         this.reset();
+        this.opcodeTable = generateOpcodeTable();
     }
 
     reset() {
@@ -17,12 +22,59 @@ export class Cpu {
 
     step() {
         const opcode = this.readNextByte();
-        console.log(
-            `Executing opcode: ${opcode.toString(16).padStart(2, '0')}`,
-        );
+        const operation = this.opcodeTable.get(opcode);
+
+        if (!operation) {
+            throw new Error(`Unknown opcode: ${opcode.toString(16)}`);
+        }
+
+        operation(this);
     }
 
     readNextByte() {
         return this.memoryBus.read(this.registers.pc++);
     }
+
+    getR8Value(reg: R8Register): number {
+        if (reg === '[hl]') {
+            return this.memoryBus.read(this.registers.hl);
+        }
+
+        return this.registers[reg];
+    }
+
+    setR8Value(reg: R8Register, value: number) {
+        if (reg === '[hl]') {
+            this.memoryBus.write(this.registers.hl, value);
+        } else {
+            this.registers[reg] = value;
+        }
+    }
+}
+
+function generateOpcodeTable() {
+    const table = new Map<number, (cpu: Cpu) => void>();
+
+    // NOP
+    table.set(0x00, () => {});
+
+    // generate ADD A, r8 handlers
+    for (let i = 0; i < 8; i++) {
+        const opcode = 0x80 + i;
+
+        table.set(opcode, cpu => {
+            const value = cpu.getR8Value(r8Registers[i]);
+            const sum = cpu.registers.a + value;
+            cpu.registers.a = sum & 0xff;
+
+            // update flags
+            cpu.registers.zeroFlag = cpu.registers.a === 0 ? 1 : 0;
+            cpu.registers.subtractFlag = 0;
+            cpu.registers.halfCarryFlag =
+                (cpu.registers.a & 0xf) + (value & 0xf) > 0xf ? 1 : 0;
+            cpu.registers.carryFlag = sum > 0xff ? 1 : 0;
+        });
+    }
+
+    return table;
 }
